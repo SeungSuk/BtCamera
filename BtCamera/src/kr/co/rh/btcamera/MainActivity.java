dac091df027825fad272b77e09ed0d7c55b5fd0d
SeungSuk <tmdtjrdl@gmail.com>
2014-07-20 17:30:22 +0900

    sssss

---
+++ BtCamera/src/kr/co/rh/btcamera/MainActivity.java
@@ -0,0 +1,18 @@
+package kr.co.rh.btcamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //fff  dddddd
+        Log.e("ssryu", "testr");
+    }
+}
