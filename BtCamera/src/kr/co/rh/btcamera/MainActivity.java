f1b09999ed465f141e2fa3d48e8e82dec89e01ca
keirux <keirux@keirux-PC>
2014-07-22 15:53:46 +0900

    test

--- BtCamera/src/kr/co/rh/btcamera/MainActivity.java
+++ BtCamera/src/kr/co/rh/btcamera/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         //fff  dddddd
         Log.e("ssryu", "testr");
+        //test
+        
     }
 }
