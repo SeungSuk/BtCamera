df0addda56ee46dcfb1b5318e5afe922ae735214
SeungSuk <SeungSuk@172.30.27.178>
2014-07-26 13:39:12 +0900

    btn추

--- BtCamera/src/kr/co/rh/btcamera/MainActivity.java
+++ BtCamera/src/kr/co/rh/btcamera/MainActivity.java
@@ -3,8 +3,8 @@ package kr.co.rh.btcamera;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -21,6 +21,14 @@ public class MainActivity extends Activity {
         //aaaaaaa
         
         Button btn = new Button(this);
+        btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
         
     }
 }
