e33eb058b725c34ad70d676762163ff02c7e2d1a
SeungSuk <SeungSuk@172.30.27.178>
2014-07-26 14:46:12 +0900

    dd

--- BtCamera/src/kr/co/rh/btcamera/MainActivity.java
+++ BtCamera/src/kr/co/rh/btcamera/MainActivity.java
@@ -48,6 +48,8 @@ public class MainActivity extends Activity {
         //rrrrr
         TextView tv = null;
         //asdfssddd
-        //ggggggg
+        //adsfadsf
+        //ggggdddggg
+        //asdfddasdfasdfsadf
     }
 }
